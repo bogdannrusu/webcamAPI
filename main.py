d641ed16c6dbc7eaf79c5723729b03bf7e11eb42
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-04-23 11:51:55 +0300

    refactoring api from python camera to javascript accesing camera

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import cv2
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='src')
 
@@ -7,26 +6,6 @@ app = Flask(__name__, template_folder='src')
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-def gen_frames():
-    camera = cv2.VideoCapture(0)
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            if not ret:
-                break
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 if __name__ == "__main__":
