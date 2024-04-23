d52b6a66a26f54fd74ce84934d588b87486dcf86
Bogdan Rusu <81962952+BogdannRusu@users.noreply.github.com>
2024-04-23 11:43:18 +0300

    Refactoring program for succesess deployment

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import io
-import logging
-import time
+import cv2
 from flask import Flask, render_template, Response
-from picamera import PiCamera
 
 app = Flask(__name__, template_folder='src')
-
-
-app.logger.setLevel(logging.DEBUG)
-
-camera = PiCamera()
-camera.resolution = (640, 480)
-
-
-def gen_frames():
-    stream = io.BytesIO()
-    for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
-        stream.seek(0)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
-        stream.seek(0)
-        stream.truncate()
 
 
 @app.route('/')
@@ -31,6 +12,21 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                break
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 if __name__ == "__main__":
